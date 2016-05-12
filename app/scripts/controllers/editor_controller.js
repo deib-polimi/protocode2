e863cc01ee89166c963b6372b7d288954f0e1917
polimi <polimi@polimis-MacBook-Air.local>
2016-05-12 13:08:40 +0200

    salvataggio su safari

--- app/scripts/controllers/editor_controller.js
+++ app/scripts/controllers/editor_controller.js
@@ -1,14 +1,26 @@
 App.EditorController = Ember.ObjectController.extend(App.Saveable, {
 
-    actions: {
-      generateAppModel: function() {
-        this.get('model').toXml().then(function (model) {
-          var xmlString = '<?xml version="1.0" encoding="UTF-8"?>\n' + new XMLSerializer().serializeToString(model.documentElement);
+  actions: {
+    generateAppModel: function() {
+      this.get('model').toXml().then(function (model) {
+        var xmlString = '<?xml version="1.0" encoding="UTF-8"?>\n' + new XMLSerializer().serializeToString(model.documentElement);
+        if(navigator.userAgent.toLowerCase().indexOf("safari") > -1) {
+          window.alert("Safari browser has been detected!\nPlease type ⌘+S and save the page as\nExport As: protocode.xmi\nFormat: Page Source");
           var blob = new Blob(
             [vkbeautify.xml(xmlString)],
-            {type: "application/xml;charset=ASCII"});
-          saveAs(blob, "protocode.xmi");
-        });
-      }
+            {type: "text/xml;charset=ASCII"}
+          );
+        } else {
+          var blob = new Blob(
+            [vkbeautify.xml(xmlString)],
+            {type: "application/xml;charset=ASCII"}
+          );
+        }
+        saveAs(blob, "protocode.xmi");
+      });
+
     }
+
+  }
+
 });
