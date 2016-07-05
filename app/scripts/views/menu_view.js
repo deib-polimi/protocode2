0ec379907f0ea071893427eaa17ad8b324109cc6
aldopolimi <aldo.pintus@gmail.com>
2016-07-06 04:53:30 +0800

    menu visibile solo in launcher per android

--- app/scripts/views/menu_view.js
+++ app/scripts/views/menu_view.js
@@ -2,6 +2,23 @@ App.MenuView = Ember.View.extend({
   tagName: 'div',
   classNames: ['app-menu'],
   classNameBindings: ['controller.controllers.editor.smartphone.platform'],
-  templateName: 'views/menu_view'
+  templateName: 'views/menu_view',
+
+  attributeBindings: ['style'],
+
+  style: function() {
+    var isAndroid = this.get('controller.controllers.editor.smartphone.platform') == 'android';
+    var isLauncher = this.get('context.launcher') == true;
+    style = "";
+
+    if(isAndroid && !isLauncher) {
+        style += "display:none";
+    }
+
+    return style;
+  }.property(
+    'controller.controllers.editor.smartphone.platform',
+    'context.launcher'
+  )
 
 });
